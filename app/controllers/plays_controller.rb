4beae77234868523d43c9e09ef7c93a55531f99b
Joseph <unedujoe@gmail.com>
2016-07-25 20:18:13 +0000

    Added simple form

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,4 +1,19 @@
 class PlaysController < ApplicationController
     def index
     end
+    
+    
+    def new
+        @play = Play.new
+    end
+    
+    
+    def create
+        @play = Play.new(play_params)
+    end
+    
+    private 
+    def play_params
+    (params_require(:play).permit(:title,  :description, :director))
+    end  
 end
